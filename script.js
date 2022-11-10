6ba3e10f5cec86c954193183d9d8015af6ab5c10
ben.ip <ben.ip@ha.org.hk>
2022-11-10 23:38:38 +0800

    no message

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var eif_setup_hcp_config = "/appl/eif/home/eifadm/script/EIFEXP/HCP_SFTP_CONFIG/eif_setup_hcp_config.ksh ";
+var sftp_config = "/appl/eif/home/eifadm/script/EIFEXP/HCP_SFTP_CONFIG/eif_setup_hcp_sftp_config.ksh";
+var eif_setup_hcp_job_control = "/appl/eif/home/eifadm/script/EIFEXP/HCP_SFTP_CONFIG/eif_setup_hcp_job_control.ksh";
+
 function auto_grow(element) {
     element.style.height = "5px";
     element.style.height = (element.scrollHeight)+"px";
@@ -15,22 +19,28 @@ function onload() {
             this.style.height = this.scrollHeight + 'px';
         }
 
-
+	
 function generate() {
     HcpID = document.getElementById("HcpID").value;
+	sftp = "0";
+	inbox = "1";
+	Env = document.getElementById("Env").value;
+	
     EffectiveDay = document.getElementById("EffectiveDay").value;
-    Env = document.getElementById("Env").value;
+    
 
     console.log("Start generate()");
     console.log("-->"+HcpID + EffectiveDay + Env);
 
     //result = HcpID + EffectiveDay + Env;
     if (Env == "UAT"){
+		
         var result = "";
         var result2 ="";
-        for (let i = 1; i <= 2; i++) {
-            result += `>XXX.sh -h "${HcpID}" -f "${EffectiveDay}" -s "P" "${i}" -e "${Env}"`+ "<br>";
-            result2 += `>XXX.sh -h "${HcpID}" -f "${EffectiveDay}" -s "S" "${i}" -e "${Env}"`+ "<br>";
+        for (let i = 1; i <= 1; i++) {
+            //result += eif_setup_hcp_config +`-h "${HcpID}" -f "${EffectiveDay}" -s "P" "${i}" -e "${Env}"`+ "<br>";
+			
+			result += eif_setup_hcp_config+"-t "+'"NEW"' +` -h "${HcpID}" -p "${sftp}" -i "${inbox}" -a `+'"19000101" -d "29000101" '+ `-e "${Env}"`+ ' -s "P" -y "EXP";';
             //console.log("result"+[i]+" : "+result + result2)
         }    
         document.getElementById("output").innerHTML = result+result2;
@@ -38,10 +48,10 @@ function generate() {
     }else if (Env == "PRD"){
        var result = "";
          var result2 ="";
-        for (let i = 1; i <= 2; i++) {
-            result  += ` >XXX.sh -h "${HcpID}" -f "${EffectiveDay}" -s "P" "${i}" -e "${Env}"\n`;
-            result2 += ` >XXX.sh -h "${HcpID}" -f "${EffectiveDay}" -s "S" "${i}" -e "${Env}"\n`;
-            //console.log("result"+[i]+" : "+result + result2)
+        for (let i = 1; i <= 1; i++) {
+            result += eif_setup_hcp_config+"-t "+'"NEW"' +` -h "${HcpID}" -p "${sftp}" -i "${inbox}" -a `+'"19000101" -d "29000101" '+ `-e "${Env}"`+ ' -s "P" -y "EXP"\n';
+            result += eif_setup_hcp_config+"-t "+'"NEW"' +` -h "${HcpID}" -p "${sftp}" -i "${inbox}" -a `+'"19000101" -d "29000101" '+ `-e "${Env}"`+ ' -s "S" -y "EXP"\n';
+			//console.log("result"+[i]+" : "+result + result2)
         }    
         document.getElementById("result").innerHTML = result+result2;
         document.getElementById("output").innerHTML = result+result2;
