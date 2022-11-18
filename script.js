4da7686f62b1000a862f94b97ae19c3371c1c885
ben.ip <ben.ip@ha.org.hk>
2022-11-18 21:34:09 +0800

    test

--- script.js
+++ script.js
@@ -28,54 +28,52 @@ function generate() {
 	
     EffectiveDay = document.getElementById("EffectiveDay").value;
     
+	//console.log("Start generate()");
+    //console.log("-->"+HcpID + EffectiveDay + Env);
 
-    console.log("Start generate()");
-    console.log("-->"+HcpID + EffectiveDay + Env);
-
-    //result = HcpID + EffectiveDay + Env;
     if (Env == "UAT"){
 		
         var result = "";
         var result2 ="";
-        for (let i = 1; i <= 1; i++) {
+        for (let i = 1; i <= 2; i++) {
             //result += eif_setup_hcp_config +`-h "${HcpID}" -f "${EffectiveDay}" -s "P" "${i}" -e "${Env}"`+ "<br>";
-			
-			result += eif_setup_hcp_config+"-t "+'"NEW"' +` -h "${HcpID}" -p "${sftp}" -i "${inbox}" -a `+'"19000101" -d "29000101" '+ `-e "${Env}"`+ ' -s "P" -y "EXP";';
+			//result += eif_setup_hcp_config+"-t "+'"NEW"' +` -h "${HcpID}" -p "${sftp}" -i "${inbox}" -a `+'"19000101" -d "29000101" '+ `-e "${Env}"`+ ' -s "P" -y "EXP"';
+			//result2 += eif_setup_hcp_config+"-t "+'"NEW"' +` -h "${HcpID}" -p "${sftp}" -i "${inbox}" -a `+'"19000101" -d "29000101" '+ `-e "${Env}"`+ ' -s "S" -y "EXP"';
             //console.log("result"+[i]+" : "+result + result2)
         }    
-        document.getElementById("output").innerHTML = result+result2;
+        document.getElementById("output").innerHTML = result2;
         //console.log(result);
     }else if (Env == "PRD"){
        var result = "";
          var result2 ="";
-        for (let i = 1; i <= 1; i++) {
-            result += eif_setup_hcp_config+"-t "+'"NEW"' +` -h "${HcpID}" -p "${sftp}" -i "${inbox}" -a `+'"19000101" -d "29000101" '+ `-e "${Env}"`+ ' -s "P" -y "EXP"\n';
-            result += eif_setup_hcp_config+"-t "+'"NEW"' +` -h "${HcpID}" -p "${sftp}" -i "${inbox}" -a `+'"19000101" -d "29000101" '+ `-e "${Env}"`+ ' -s "S" -y "EXP"\n';
+        for (let i = 1; i <= 3; i++) {
+            result += eif_setup_hcp_config+"-t "+'"NEW"' +` -h "${HcpID}" -p "${sftp}" -i "${inbox}" -a `+'"19000101" -d "29000101" '+ `-e "${Env}"`+ ' -s "P" -y "EXP"'+ "\n";
+            result += eif_setup_hcp_config+"-t "+'"NEW"' +` -h "${HcpID}" -p "${sftp}" -i "${inbox}" -a `+'"19000101" -d "29000101" '+ `-e "${Env}"`+ ' -s "S" -y "EXP"'+ "\n";
 			//console.log("result"+[i]+" : "+result + result2)
         }    
-        document.getElementById("result").innerHTML = result+result2;
-        document.getElementById("output").innerHTML = result+result2;
+        document.getElementById("output").innerHTML = result;
         //console.log(result);
-    }
-    /*
-    if (HcpID.length==0 || EffectiveDay.length==0 || Env.length==0)
-        console.log(" null ");
-        return;
-        */
-    //result = `　　${HcpId}  ===  ${EffectiveDay}是怎么回事呢？${Env}\r\n`;
+    }else if (Env != "PRD" || Env != "UAT"){
+		document.getElementById("output").innerHTML = "Please input ENV";
+	}
+}
 
-   
-
-    //document.getElementById("result").value = result;
-    //auto_grow(document.getElementById("result"));
-
-
-}    /*
-    if (HcpId.length==0 || EffectiveDay.length==0 || Env.length==0)
-        return;
-    result = `　　${HcpId}  ===  ${EffectiveDay}是怎么回事呢？${Env}\r\n`;
-    document.getElementById("result").value = result;
-    auto_grow(document.getElementById("result"));
-    */
-
-
+function reset(){
+	result = "....";
+	console.log(result);
+	document.getElementById("output").innerHTML = result;
+	document.getElementById("output1").innerHTML = result;
+	document.getElementById("output2").innerHTML = result;
+}
+     
+	  function downloadFile(){
+         const link = document.createElement("a");
+		 //document.getElementById("output").innerHTML = eif_setup_hcp_config;
+         const content = document.getElementById("output").value;
+		 console.log(content);
+         const file = new Blob([content], { type: 'text/plain' });
+         link.href = URL.createObjectURL(file);
+         link.download = "sample.txt";
+         link.click();
+         URL.revokeObjectURL(link.href);
+      };
